5a04ce48f27682294892dbde28a5c032b3b277e3
Arnab Pal <109953155+arnabpal16@users.noreply.github.com>
2023-09-10 14:08:12 +0530

    ...

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const reviews = [
   {
     id: 1,
-    name: "Pranay Gupta",
+    name: "Aman Pal",
     job: "Software Engineer",
     image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
     text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
@@ -15,21 +15,21 @@ const reviews = [
   },
   {
     id: 3,
-    name: "Soumya Banerjee",
+    name: "Ayan Banerjee",
     job: "Marketing Manager",
     image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
     text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
   },
   {
     id: 4,
-    name: "Saikat Mukherjee",
+    name: "Subham Mukherjee",
     job: "Content Strategist",
     image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
     text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
   },
   {
     id: 5,
-    name: "Aritra Biswas",
+    name: "Niladri Adak",
     job: "Fuck Boy",
     image: "https://avatars.githubusercontent.com/u/93366359?v=4",
     text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
